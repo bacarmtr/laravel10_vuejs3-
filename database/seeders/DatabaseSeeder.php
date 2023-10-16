466f84d09685466a1dc3c67eaabe74aa5fa1927f
eagle <bacarmtr@yahoo.fr>
2023-10-16 16:37:29 +0200

    Utilisalition de Task Collection pour afficher les data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace Database\Seeders;
+use App\Models\Task;
+use Illuminate\Database\Seeder;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Task::factory(10)->create();
     }
 }
